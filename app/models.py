e9c1fe342c2f1991c89a7fa181d84147f835a295
zhangmm <zhangmin6105@qq.com>
2015-10-26 12:00:43 +0800

    add api authentication
    Signed-off-by: zhangmm <zhangmin6105@qq.com>

--- app/models.py
+++ app/models.py
@@ -300,6 +300,19 @@ class User(UserMixin, db.Model):
                 db.session.add(user)
                 db.session.commit()
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     def __repr__(self):
         return '<User %r>' % self.username
 
