8b3a174d8a0f566038ee383f91ac5d011c8ec72d
zhangmm <zhangmin6105@qq.com>
2015-10-27 14:13:29 +0800

    add coverage test
    Signed-off-by: zhangmm <zhangmin6105@qq.com>

--- manage.py
+++ manage.py
@@ -10,6 +10,13 @@ from flask.ext.migrate import Migrate, MigrateCommand
 
 __author__ = 'zhangmm'
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 app = create_app(os.getenv('ZBLOG_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -24,11 +31,25 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests"""
+    if coverage and os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'temp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
